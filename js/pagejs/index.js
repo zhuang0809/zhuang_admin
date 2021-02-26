8f5ea1609a3c64e46f4d02a2a872bf56b2d006f5
znt <znt1632251167@163.com>
2021-02-26 17:36:18 +0800

    2021/2/26修改

--- js/pagejs/index.js
+++ js/pagejs/index.js
@@ -5,6 +5,9 @@ var mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
     effect: 'fade',
+    autoplay: {
+        disableOnInteraction: false,
+    },
     // 如果需要分页器
     pagination: {
         el: '.swiper-pagination',
@@ -14,12 +17,7 @@ var mySwiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // 如果需要滚动条
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-})       
+})
 var mySwiper2 = new Swiper('.swiper-container2', {
     autoplay: true,
     delay: 10000,
@@ -27,18 +25,38 @@ var mySwiper2 = new Swiper('.swiper-container2', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
     effect: 'fade',
-    // 如果需要分页器
-    // pagination: {
-    //     el: '.swiper-pagination2',
-    // },
-    // // 如果需要前进后退按钮
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
+    autoplay: {
+        disableOnInteraction: false,
+    },
+})  
 
-    // 如果需要滚动条
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-})     
+var mySwiper3 = new Swiper('.swiper-container3', {
+    autoplay: true,
+    delay: 15000,
+    speed: 5000,
+    slidesPerView : 3,  
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+    effect: 'coverflow',
+    autoplay: {
+        disableOnInteraction: false,
+    },
+}) 
+
+var mySwiper4 = new Swiper('.swiper-container4', {
+    autoplay: true,
+    delay: 15000,
+    speed: 5000,
+    slidesPerView : 4,  
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+    effect: 'slide',
+    autoplay: {
+        disableOnInteraction: false,
+    },
+}) 
+// 企业荣誉
+$(function(){
+    var box = $(".content_wrap")
+
+})
